7eb2dc5337395a7e6b849702e412d2f5c7ed504a
joakG16 <4379@holbertonschool.com>
2022-08-23 07:29:42 -0700

    border case (NULL) added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,6 +15,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int flag = 0;
 
+	if (!array || size == 0)
+		return;
+
 	for (; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
